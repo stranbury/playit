99b2fdefd278c97ff299bda6c523f7a45d96b3e4
Darry <darry.jdp@gmail.com>
2016-01-04 21:21:54 +0100

    change the home

--- src/app/movieDetail/controller.js
+++ src/app/movieDetail/controller.js
@@ -7,18 +7,9 @@ export class MovieDetailController {
     this.background = backdrop;
     this.movie = movie;
     $log.debug(this.movie);
-    this.preview = true;
+    this.preview = false;
     this.link = 'OPmOXJtxxoo';
-    this.youtube = _youtubeVideo;
     $log.log( this.youtube  );
-    youtubeVideo('sl1Q6W0UzGk', {
-  elementId: 'youtube',
-  width: 640,
-  height: 390,
-  autoplay: true,
-  controls: false
-})
-
   }
 
 
